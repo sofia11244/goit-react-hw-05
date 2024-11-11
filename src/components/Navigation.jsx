1ac1840f28384f453aca5e6ab6cffc74353425be
sofia11244 <denizsofia03@gmail.com>
2024-11-12 02:45:38 +0300

    go back button improvised uselocaiton added in nav file

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 // import propTypes from 'prop-types';
 
-import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate, useLocation } from 'react-router-dom';
+import { useRef, useEffect } from 'react';
 
 import style from './comp-style/Navigation.module.css'
 import clsx from 'clsx';
@@ -14,6 +15,13 @@ const buildLinkClass = ({ isActive }) => {
 //   to add go back button we need to use uselocation hook <3
 
 function Navigation() {
+
+    const location = useLocation();
+    const prevLocation = useRef(location.state?.from || '/'); // 'from' state'ini kullanarak geri gitme işlemi
+
+  useEffect(() => {
+   prevLocation.current = location.state?.from || '/'; // Her sayfa değişiminde previous location'ı güncelle
+  }, [location]);
     return (
         <div>
             
@@ -34,7 +42,10 @@ function Navigation() {
 
 function BackButton() {
   const navigate = useNavigate();
-
+  const location = useLocation();
+  if (location.pathname === '/') {
+    return null;
+  }
   return (
     <>
       <button onClick={() => navigate(-1)}>go back</button>
